6003b6b51bbce431fee3ccf0edb07af4ad9f8c79
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2023-04-10 20:59:13 -0300

    Add files via upload

---
+++ comboSwitch.js
@@ -0,0 +1,71 @@
+//NEEDS TUNNING
+
+let hits = 1;
+
+switch (hits) {
+    case 2:
+        console.log("Basic combo!");
+        break;
+    case 3:
+        console.log("Triple Combo!");
+        break;
+    case 4:
+        console.log("Quad Combo!");
+        break;
+    case 5:
+        console.log("Solid Combo!");
+        break;
+    case 6:
+        console.log("Hyper Combo!");
+        break;
+    case 7:
+        console.log("Brutal Combo!");
+        break;
+    case 8:
+        console.log("Master Combo!");
+        break;
+    case 9:
+        console.log("Blaster Combo!");
+        break;
+    case 10:
+    case 11:
+    console.log("Extreme Combo!");
+        break;
+    case 12:
+    case 13:
+    console.log("Awesome Combo!");
+        break;
+    case 14:
+    case 15:
+    console.log("MONSTER Combo!");
+        break;
+    case 16:
+    case 17:
+    console.log("INSANE Combo!");
+        break;
+    case 18:
+    case 19:
+    case 20:
+    console.log("BEASTLY Combo!");
+        break;
+    case 21:
+    case 22:
+    case 23:
+    console.log("KIIING Combo!");
+        break;
+    case 24:
+    case 25:
+    case 26:
+    console.log("CRAZY Combo!");
+        break;
+    case 27:
+    case 28:
+    case 29:
+    console.log("KILLER Combo!");
+        break;
+    case 30:
+    console.log("GODLIKE COMBO!");
+        break;
+    default:
+        console.log("ULTRAAAAAA!")
+    }
