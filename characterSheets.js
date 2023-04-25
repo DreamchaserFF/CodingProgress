9ee072fd0e5fc541589bc385cc706ee0113527fb
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2023-04-24 22:03:48 -0300

    Add files via upload

---
+++ characterSheets.js
@@ -0,0 +1,66 @@
+// Character data: .alias, .birthplace, .legacy: legacyName, legacyEffect, .objective, personality
+
+const ayla = {
+    fullName    : "Ayla Ashwood",
+    ocupation   : "Guardian and protector of a community of ostracized mystics and their families.",
+    alias       : "The Chimera",
+    birthplace  : "Ashwood Forest",
+    appearence  : "Ayla is a medium height young female, she has long pitch black hair, pale skin,\
+ brown eyes (red during legacy use) and has a fairly muscular figure.",
+    legacy : { 
+        legacyName  : "Shadows in the Night",
+        effect      : "Ayla is capable of using the habilities and changing her appearence to that of\
+ one of the thousands of creatures that live inside her soul.",
+        sideEffect  : "Auditory hallucinations; risk of going berserk; requires special medicine to\
+ maintain control of her own body."
+},
+    abilities   : "Exceptional hunter, extensively knowledgeable about wildlife and mystic creatures\
+ super human strenght and agility, extremely dangerous at close quarters due to her claws and fangs.",
+    objective   : "Ayla wishes to stop the decay of magic and save the mystic races from extinction.",
+    personality : "Cheerful, hopeful, kind, irreverent."
+};
+
+const tristan = {
+    fullName    : "Tristan Blake",
+    ocupation   : "Wanted fugitive.",
+    alias       : "The Exiled",
+    birthplace  : "Atrozzia",
+    appearence  : "Tristan is a medium height young white skinned male, he has greyish black short hair,\
+ grey eyes, slender build, his body is covered in scars and his right arm has mutated into having a\
+ bizzarre metallic look.",
+    legacy : {
+        legacyName  : "Redemption",
+        effect      : "Tristan's mutated arm can generate incredible ammounts of energy, but tristan is\
+ incapable of controlling it. Due to his Legacy, Tristan is inherently dangerous and should not be\
+ approached in any circunstance.",
+        sideEffect  : "Causes intense pain to the user after use; slowly mutates the user's body, and\
+ will eventually cause their death."
+    },
+    abilities   : "Hand to hand specialist, sharpshooter, excelent interrogator and torturer (swore to\
+ never use these skills again), he has good medical skills and is a natural tactician.",
+    objective   : "Tristan's sole objective is to find a way to completely destroy his own Legacy so\
+ no one else will inherit it and become burdened with it in the event of his death.",
+    personality : "Quiet, gloomy, humble, tries to appear happy at all times."
+}  
+
+const aria = {
+    fullName    : "Aria Garland I",
+    ocupation   : "Sole ruler of the nation of Thargein.",
+    alias       : "The Silver Light of Thargein",
+    birthplace  : "Thargein",
+    appearence  : "Aria is a short young woman, she has white skin, pale blonde medium hair with\
+ characteristic shoulder height bangs, she always wear her royal armors and bears herself in a proud\
+ manner.",
+    legacy : { 
+        legacyName  : "None. Aria did not inherit her father's Legacy, making her the first of the\
+ Garland bloodline to not have a legacy.",
+        effect      : "None",
+        sideEffect  : "Not having a legacy has caused Aria to lose the faith of her people, most believe\
+ that her not having a Legacy is proof that she's incapable of leading her people, that she's\
+ to blame for the decline of the nation and that she shouldn't have been crowned queen after her\
+ father passed. Even though she's a kind and benevolent ruler."
+},
+    objective   : "Aria dreams of restoring the nation of Thargein to it's former glory and to find\
+ a cure for her beloved's misterious disease before it is too late.",
+    personality : "Serious, stern, kind-hearted, respectiful, proud."
+};
