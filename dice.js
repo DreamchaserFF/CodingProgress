318a4922a2c830cd9e940291906b69931cf247d6
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2023-07-06 07:21:14 -0300

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,7 +1,15 @@
 //console.log(Math.floor(Math.random() * 6) + 1);
 
-function rollDie(sides)
-{
-    return console.log(Math.floor(Math.random() * sides) + 1);
-}
-return rollDie(20);
+//function rollDie(sides)
+//{
+//    return console.log(Math.floor(Math.random() * sides) + 1);
+//}
+//return rollDie(20);
+
+// 2.0
+
+function dado(lados){
+    let valor = Math.floor(Math.random() * lados) + 1;
+    let mensagem = console.log(valor);
+        alert("Você tirou: " + valor);
+};
