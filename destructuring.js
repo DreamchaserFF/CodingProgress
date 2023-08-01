a388803af95f1eadb20e637dcd5c91b400445f7b
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2023-08-01 13:04:23 -0300

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -1,4 +1,4 @@
- const raceResults = [
+const raceResults = [
     "Eliud Kipchoge",
     "Feyisa Lelisa",
     "Galen Rupp",
@@ -14,3 +14,85 @@
  const [gold, silver, bronze] = raceResults;
  const [first, , ,fourth] = raceResults;
  const [winner, ...others] = raceResults;
+
+//OBJECT DESTRUCTURING
+
+//  const runner = {
+//    first: "Eliud",
+//    last: "Kipchoge",
+//    country: "Kenya",
+//    title: "Elder of the Order of the Golden Heart of Kenya"
+//  };
+
+// const {first, last, time} = runner;
+
+// const {
+//    country: nation,
+//    title: honorific
+// } = runner
+
+// const {
+//    first,
+//    last,
+//    ...other
+// } = runner;
+
+
+//NESTED DESTRUCTURING
+
+// const goddesses = [{
+//    name: "Astra",
+//    domain: "Goddess of Fire",
+//    alias: "The Witch Queen"
+// },
+// {
+//    name: "Shion",
+//    domain: "Goddess of Time",
+//    alias: "The Keeper of Fate"
+// },
+// {
+//    name: "Orochi",
+//    domain: "Goddess of Culture and Pleasures",
+//    alias: "The Demon Empress"
+// }]
+
+// const [{alias: deity}, ,{name: trueName}] = goddesses;
+
+// const [,whiteLady] = goddesses;
+// const {info} = whiteLady;
+
+
+const deity = {
+   name: "Astra",
+   domain: "Goddess of Fire",
+   alias: "The Witch Queen"
+}
+
+// function print(goddess) {
+//    const{
+//       name,
+//       domain,
+//       alias
+//    } = goddess;
+//    console.log(`${name}, the ${domain}, also known as ${alias}.`)
+// };
+
+
+// function print({
+//       name,
+//       domain,
+//       alias
+//    }) {
+//    console.log(`${name}, the ${domain}, also known as ${alias}.`)
+// };
+
+
+const demon = [
+   "Orochi",
+   "Goddess of Culture and Pleasures",
+   "The Demon Empress",
+]
+
+function info([name, domain, title]) {
+   console.log(`Identity: ${name}, the ${domain}.`);
+}
