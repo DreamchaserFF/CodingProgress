4f05cf3649af5d16d516b408d8b6a2b8b75cc18e
DreamchaserFF <118946745+DreamchaserFF@users.noreply.github.com>
2023-08-07 13:11:51 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,29 @@
+// const calc = {
+//     soma: function (x,y){
+//         return x + y;
+//     },
+//     sub: function (x,y){
+//         return x - y;
+//     },
+//     mult: function (x,y){
+//         return x * y;
+//     },
+//     div: function (x,y){
+//         return x / y;
+//     },
+// }
+
+const calc = {
+    soma(x,y) {
+        return x + y
+    },
+    sub(x,y) {
+        return x - y
+    },
+    mult(x,y) {
+        return x * y
+    },
+    div(x,y) {
+        return x / y
+    },
+}
